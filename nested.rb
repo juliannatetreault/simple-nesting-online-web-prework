bd44917919fb9f31b3912181663b990263fcc25e
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-17 04:00:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:known_for][:languages].to_s
+  programmer_hash[:dennis_ritchie][:languages]
   programmer_hash
 end
 
